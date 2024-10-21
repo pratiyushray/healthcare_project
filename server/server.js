1e7e3c007d6b3ee9fd0049bf8d6d987de44758e9
pratiyush ray <pkray0021@gmail.com>
2024-10-22 01:13:25 +0530

    Repo Initialization, DB Connection Local

---
+++ server/server.js
@@ -0,0 +1,29 @@
+// FRAMEWORK CONFIGURATION
+const express = require("express");
+const connectDb = require("./config/dbConnection");
+const errorHandler = require("./middleware/errorHandler");
+const cors = require("cors");
+
+// env file config
+const dotenv = require("dotenv").config();
+
+connectDb();
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cors());
+// Error handling middleware
+app.use(errorHandler);
+
+
+// ROUTES BELOW
+app.get('/', (req, res) => {
+    res.send("working");
+});
+
+
+// APP CONFIG START
+app.listen(port, () => {
+    console.log(`Server running on port http://localhost:${port}`);
+});
